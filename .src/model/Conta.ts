8d6fc34e7e9f4e1d7c087ac4261e120cbd3b2bcd
Fern <fmenezes098@gmail.com>
2024-12-11 23:35:29 -0300

    :sparckles: Class Model Conta

---
+++ .src/model/Conta.ts
@@ -0,0 +1,101 @@
+export class Conta{
+
+    /**Definir o modelo de dados | por boas praticas no Typescript usa-se o _ na frente da palavra para indicar que ela é privada*/
+    private _numero: number;
+    private _agencia: number;
+    private _tipo: number;
+    private _titular: string;
+    private _saldo: number;
+
+	constructor(numero: number, agencia: number, tipo: number, titular: string, saldo: number) {
+		this._numero = numero;
+		this._agencia = agencia;
+		this._tipo = tipo;
+		this._titular = titular;
+		this._saldo = saldo;
+	}
+
+
+    // gerar todos os métodos get e set, com a extensão Typescript ToolBox
+
+	public get numero(): number {
+		return this._numero;
+	}
+    
+	public get agencia(): number {
+		return this._agencia;
+	}
+    
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+	public get titular(): string {
+		return this._titular;
+	}
+
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+	public set numero(value: number) {
+		this._numero = value;
+	}
+
+	public set agencia(value: number) {
+		this._agencia = value;
+	}
+
+	public set tipo(value: number) {
+		this._tipo = value;
+	}
+
+	public set titular(value: string) {
+		this._titular = value;
+	}
+
+	public set saldo(value: number) {
+		this._saldo = value;
+	}
+
+
+
+	public sacar (valor: number): boolean {
+		if(valor > this._saldo){
+			console.log("Saldo Insuficiente!");
+			return false;
+		}
+
+		this._saldo -=valor;
+		return true;
+	}
+
+	public depositar (valor: number){
+		this._saldo -=valor;
+	}
+
+	public visualizar(){
+
+		let tipo: string;
+		switch(this._tipo){
+			case 1:
+				tipo = "Conta Corrente";
+				break;
+			case 2:
+				tipo = "Conta Poupança";
+				break;
+			default:
+				tipo = "Tipo Inválido";
+		}
+		
+    //usar os métodos get e set para gerar as informações no console
+        console.log("**********************************************");
+        console.log("               DADOS DA CONTA                 ");
+        console.log("**********************************************");
+        console.log(`Numero da Conta: ${this._numero}`);
+        console.log(`Numero da Agência: ${this._agencia}`);
+        console.log(`Tipo da Conta: ${tipo}`);
+        console.log(`Nome do titular da Conta: ${this._titular}`);
+        console.log(`Saldo da Conta: ${this._saldo}`);
+	}
+}
--- menu.ts
+++ menu.ts
@@ -1,8 +1,24 @@
 import readlinesync = require("readline-sync");
 import { colors } from './.src/util/colors';
+import { Conta } from "./.src/model/Conta"
 
 export function main(){
     let opcao: number;
+
+    //criar novas contas
+    const c1 = new Conta(1, 123, 1, "Jonas", 100000);
+    c1.visualizar();
+
+    const c2 = new Conta(1, 123, 1, "Jonas", 100000);
+    c2.visualizar();
+
+    //deposito
+    console.log(c1.depositar(200));
+
+    c2.visualizar();
+    //sacar
+    console.log(c2.sacar(200));
+    c2.visualizar();
 
     while(true){
         console.log(colors.bg.black, colors.fg.green);
